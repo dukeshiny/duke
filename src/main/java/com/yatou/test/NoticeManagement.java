e553ce980b7d537fb21d1b68838ce22cbf0d35ab
duke <duketyson@126.com>
2016-12-15 17:05:18 +0800

    Notice

---
+++ src/main/java/com/yatou/test/NoticeManagement.java
@@ -0,0 +1,10 @@
+package com.yatou.test;
+
+import java.io.Serializable;
+
+public class NoticeManagement implements Serializable{
+
+	public static void main(String[] args) {
+		System.out.println("heloo ");
+	}
+}
